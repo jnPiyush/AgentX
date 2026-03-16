ae1a4bf65b9f01fa3e60523a9790fbfc64a63a40
Piyush Jain <piyushj@microsoft.com>
2026-03-16 18:01:00 -0500

    chore(release): stamp 8.3.6 [skip-issue]

--- vscode-extension/src/test/commands/initialize.test.ts
+++ vscode-extension/src/test/commands/initialize.test.ts
@@ -98,7 +98,7 @@ describe('runInitializeCommand', () => {
     fakeContext = {
       subscriptions: [],
       extensionUri: vscode.Uri.file('/test/extension'),
-      extension: { packageJSON: { version: '8.3.5' } },
+      extension: { packageJSON: { version: '8.3.6' } },
     } as unknown as vscode.ExtensionContext;
     fakeAgentx = {} as unknown as sinon.SinonStubbedInstance<AgentXContext>;
     originalWorkspaceFolders = vscode.workspace.workspaceFolders;
